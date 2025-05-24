2c0d3f81ac879198767f10a64e30da51f8cf57ac
omer4ruq <omerfaruq4@gmail.com>
2025-05-24 17:33:56 +0600

    add section form desgin

--- src/components/ClassManagement/AddSection.jsx
+++ src/components/ClassManagement/AddSection.jsx
@@ -92,7 +92,7 @@ const AddSection = () => {
 
         {/* Form to Add Section */}
         <div className="bg-white border border-gray-200 p-6 rounded-lg mb-8">
-          <form onSubmit={handleSubmit} className="flex gap-4 items-center">
+          <form onSubmit={handleSubmit} className="grid grid-cols-1 md:grid-cols-4 gap-4 ">
             <div className="relative border-2 border-purple-700 rounded-lg p-4 flex-1">
               <label
                 htmlFor="sectionName"
