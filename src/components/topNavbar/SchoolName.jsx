3e2f1efeb5aa41db17e07ef94e94b460b2f81f45
omer4ruq <omerfaruq4@gmail.com>
2025-05-24 17:46:18 +0600

    navbar responsive

--- src/components/topNavbar/SchoolName.jsx
+++ src/components/topNavbar/SchoolName.jsx
@@ -3,9 +3,9 @@ export default function SchoolName() {
   return (
     <div className="flex items-center gap-2">
       <div className="bg-white w-7 sm:w-8 h-7 sm:h-8 p-1 sm:p-[6px] rounded-full flex items-center justify-center">
-        <FaSchool className="w-5 sm:w-7 h-5 sm:h-7" />
+        <FaSchool className="w-4 sm:w-7 h-4 sm:h-7" />
       </div>
-      <h3 className="text-white text-lg hidden sm:block">
+      <h3 className="text-white text-base md:text-lg hidden sm:block">
         Urban International International School and College
       </h3>
     </div>
