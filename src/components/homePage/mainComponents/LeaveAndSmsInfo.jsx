7923e1e7696b796dd021313203c6fcc158996904
Fardus-Hassan <webdev.fardus@gmail.com>
2025-07-16 19:29:38 +0600

    modify dashboard

--- src/components/homePage/mainComponents/LeaveAndSmsInfo.jsx
+++ src/components/homePage/mainComponents/LeaveAndSmsInfo.jsx
@@ -69,6 +69,45 @@ export default function LeaveAndSmsInfo() {
       </div>
 
       {/* SMS Information */}
+      <div className="bg-black/10 backdrop-blur-sm text-sm rounded-2xl shadow-xl animate-fadeIn">
+        <SectionHeader
+          title="এসএমএস তথ্য"
+          className="bg-[#DB9E30] text-[#441a05] text-xl p-4 rounded-t-2xl shadow-md font-bold"
+        />
+        <div className="sm:flex items-center gap-4 p-4 space-y-4 sm:space-y-0">
+          {/* General SMS */}
+          <div className="p-4 bg-white/5 border border-[#9d9087]/50 rounded-2xl flex items-center sm:w-1/2 animate-fadeIn" style={{ animationDelay: '0.1s' }}>
+            <div className="w-14 h-14 flex items-center justify-center rounded-full mr-3 bg-[#441a05] animate-scaleIn">
+              <img src={generalSMS} alt="সাধারণ এসএমএস আইকন" className="w-8 h-8" />
+            </div>
+            <div className="border-l-2 border-[#9d9087] pl-3">
+              <h4 className="text-[#441a05] text-[16px] font-medium">
+                সাধারণ এসএমএস
+              </h4>
+              <h4 className="text-xl font-bold text-[#441a05]">o</h4>
+              {/* <h4 className="text-xs font-medium text-[#441a05]">
+                মেয়াদ শেষ: ১২.০৮.২০২৮
+              </h4> */}
+            </div>
+          </div>
+
+          {/* Masking SMS */}
+          <div className="p-4 bg-white/5 border border-[#9d9087]/50 rounded-2xl flex items-center sm:w-1/2 animate-fadeIn" style={{ animationDelay: '0.2s' }}>
+            <div className="w-14 h-14 flex items-center justify-center rounded-full mr-3 bg-[#441a05] animate-scaleIn">
+              <img src={maskingSMS} alt="মাস্কিং এসএমএস আইকন" className="w-8 h-8" />
+            </div>
+            <div className="border-l-2 border-[#9d9087] pl-3">
+              <h4 className="text-[#441a05] text-[16px] font-medium">
+                মাস্কিং এসএমএস
+              </h4>
+              <h4 className="text-xl font-bold text-[#441a05]">o</h4>
+              {/* <h4 className="text-xs font-medium text-[#441a05]">
+                মেয়াদ শেষ: ১২.০৮.২০২৮
+              </h4> */}
+            </div>
+          </div>
+        </div>
+      </div>
       
     </div>
   );
